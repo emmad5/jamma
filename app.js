034e86b006eb31a2563c484f0b33abe2333345b8
Kumasaka <anthonykumasaka@gmail.com>
2018-12-26 12:21:06 -0800

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express"); 
+const app = express(); 
+
+
+app.get("/", (req, res) => {
+  res.send("Hello AK!"); 
+}); 
+
+const port = process.env.PORT || 5000; 
+
+app.listen(port, () => {console.log(`Listening on port ${port}`)}); 
