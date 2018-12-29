7a0be231142711fc0cc17be90412aee12ad08e0d
Kumasaka <anthonykumasaka@gmail.com>
2018-12-28 21:25:07 -0800

    Add new config vars to heroku

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -2,3 +2,7 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   secretOrKey: process.env.SECRET_OR_KEY
 }
+// module.exports = {
+//   mongoURI: process.env.MONGO_URI,
+//   secretOrKey: process.env.SECRET_OR_KEY
+// }
