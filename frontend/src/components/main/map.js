31382c693dd05dc731a693fdb69f92459d2db2a5
Emma Deas <emmadeas9@gmail.com>
2019-01-03 09:02:53 -0800

    make map 3d

--- frontend/src/components/main/map.js
+++ frontend/src/components/main/map.js
@@ -6,17 +6,52 @@ class Map extends React.Component {
         super(props);
     }
     componentDidMount() {
-        this.map = new mapboxgl.Map({
+        const map = new mapboxgl.Map({
             container: this.mapContainer,
             style: 'mapbox://styles/mapbox/light-v9',
             zoom: 12,
-            center: [-122.431297, 37.773972]
+            center: [-122.431297, 37.773972],
+            pitch: 45,
+            bearing: -17.6
+        });
+        map.on('load', function () {
+            var layers = map.getStyle().layers;
+            var labelLayerId;
+            for (var i = 0; i < layers.length; i++) {
+                if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+                    labelLayerId = layers[i].id;
+                    break;
+                }
+            }
+
+            map.addLayer({
+                'id': '3d-buildings',
+                'source': 'composite',
+                'source-layer': 'building',
+                'filter': ['==', 'extrude', 'true'],
+                'type': 'fill-extrusion',
+                'minzoom': 15,
+                'paint': {
+                    'fill-extrusion-color': '#aaa',
+                    'fill-extrusion-height': [
+                        "interpolate", ["linear"], ["zoom"],
+                        15, 0,
+                        15.05, ["get", "height"]
+                    ],
+                    'fill-extrusion-base': [
+                        "interpolate", ["linear"], ["zoom"],
+                        15, 0,
+                        15.05, ["get", "min_height"]
+                    ],
+                    'fill-extrusion-opacity': .6
+                }
+            }, labelLayerId);
         });
         
     }
 
     componentWillUnmount() {
-        this.map.remove();
+        // map.remove();
     }
 
     render() {
