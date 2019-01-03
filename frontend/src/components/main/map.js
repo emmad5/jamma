9c2fbb0b689ac97bf0be0d073411093060a95e81
Emma Deas <emmadeas9@gmail.com>
2019-01-03 11:24:32 -0800

    make map

--- frontend/src/components/main/map.js
+++ frontend/src/components/main/map.js
@@ -47,6 +47,23 @@ class Map extends React.Component {
                 }
             }, labelLayerId);
         });
+        map.on('click', function (e) {
+            var features = map.queryRenderedFeatures(e.point, {
+                layers: ['businesses'] // replace this with the name of the layer
+            });
+
+            if (!features.length) {
+                return;
+            }
+
+            var feature = features[0];
+
+            var popup = new mapboxgl.Popup({ offset: [0, -15] })
+                .setLngLat(feature.geometry.coordinates)
+                .setHTML('<h3>' + feature.properties.title + '</h3><p>' + feature.properties.description + '</p>')
+                .setLngLat(feature.geometry.coordinates)
+                .addTo(map);
+        });
         
     }
 
