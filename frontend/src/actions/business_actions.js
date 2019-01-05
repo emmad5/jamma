ca4b070cfaafd8d2ab3b6e6fd07ff1883e5fc9f1
Josh Choi <joshuachoi0508@gmail.com>
2019-01-05 12:43:14 -0800

    Make business errors reducer

--- frontend/src/actions/business_actions.js
+++ frontend/src/actions/business_actions.js
@@ -2,6 +2,7 @@ import * as APIUtil from '../util/business_api_util';
 import jwt_decode from 'jwt-decode';
 
 export const RECEIVE_ALL_BUSINESSES = "RECEIVE_ALL_BUSINESSES";
+export const RECEIVE_BUSINESS_ERRORS = "RECEIVE_BUSINESS_ERRORS"
 
 // We'll dispatch this when our user signs in
 export const receiveAllBusinesses = businesses => ({
@@ -9,8 +10,13 @@ export const receiveAllBusinesses = businesses => ({
     businesses
 });
 
+export const receiveErrors = errors => ({
+    type: RECEIVE_BUSINESS_ERRORS,
+    errors
+});
+
 export const getBusinesses = () => dispatch => (
-    APIUtil.getBusinesses().then(() => (
+    APIUtil.getBusinesses().then((businesses) => (
         dispatch(receiveAllBusinesses(businesses))
     ), err => (
         dispatch(receiveErrors(err.response.data))
--- frontend/src/reducers/errors_reducer.js
+++ frontend/src/reducers/errors_reducer.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 import SessionErrorsReducer from './session_errors_reducer';
+import businessErrorsReducer from './business_errors_reducer'
+
 
 export default combineReducers({
-  session: SessionErrorsReducer
+  session: SessionErrorsReducer,
+  business: businessErrorsReducer
 });
