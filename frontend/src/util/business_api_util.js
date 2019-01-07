5d5ed713042f8509b9b0c17d43d0a24971e41240
Josh Choi <joshuachoi0508@gmail.com>
2019-01-07 09:36:50 -0800

    Add frontend add/edit routes

--- frontend/src/util/business_api_util.js
+++ frontend/src/util/business_api_util.js
@@ -3,3 +3,11 @@ import axios from 'axios';
 export const getBusinesses = () => {
     return axios.get('/api/businesses');
 };
+
+export const editBusiness = businessData => {
+    return axios.post('/api/businesses');
+};
+
+export const addBusiness = () => {
+    return axios.patch('/api/businesses');
+};
