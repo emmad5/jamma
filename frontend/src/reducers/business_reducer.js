c628bf29a0d7cc4ce934c99288a7e85a6bdf27ce
Josh Choi <joshuachoi0508@gmail.com>
2019-01-07 10:55:59 -0800

    Action/reducer edit

--- frontend/src/reducers/business_reducer.js
+++ frontend/src/reducers/business_reducer.js
@@ -1,18 +1,16 @@
 import {
     RECEIVE_ALL_BUSINESSES,
-    RECEIVE_ONE_BUSINESSES,
-    EDIT_ONE_BUSINESS
+    RECEIVE_ONE_BUSINESS
 } from '../actions/business_actions';
 import merge from 'lodash/merge';
 
 export default function (prevState = {}, action) {
+    Object.freeze(prevState);
     let nextState = merge({}, prevState);
     switch (action.type) {
         case RECEIVE_ALL_BUSINESSES:
-            return action.businesses.data;
-        case RECEIVE_ONE_BUSINESSES:
-            debugger;
-        case EDIT_ONE_BUSINESS:
+            return merge({}, action.businesses.data);
+        case RECEIVE_ONE_BUSINESS:
             debugger;
         default:
             return prevState;
