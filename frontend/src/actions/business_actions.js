156d567bfb00d0999a7662afb79abd6ebcae0516
Josh Choi <joshuachoi0508@gmail.com>
2019-01-08 11:00:25 -0800

    Delete editBusiness action and RECEIVE_ONE_BUSINESS dispatch action

--- frontend/src/actions/business_actions.js
+++ frontend/src/actions/business_actions.js
@@ -2,17 +2,11 @@ import * as APIUtil from '../util/business_api_util';
 
 export const RECEIVE_ALL_BUSINESSES = "RECEIVE_ALL_BUSINESSES";
 export const RECEIVE_BUSINESS_ERRORS = "RECEIVE_BUSINESS_ERRORS";
-export const RECEIVE_ONE_BUSINESS = "RECEIVE_ONE_BUSINESS";
 
 
 export const receiveAllBusinesses = businesses => ({
     type: RECEIVE_ALL_BUSINESSES,
     businesses
-});
-
-export const receiveOneBusiness = business => ({
-    type: RECEIVE_ONE_BUSINESS,
-    business
 });
 
 export const receiveErrors = errors => ({
@@ -30,12 +24,5 @@ export const getBusinesses = () => dispatch => (
 
 export const addBusiness = businessData => dispatch => (
     APIUtil.addBusiness(businessData)
-    .then(business => (dispatch(receiveOneBusiness(business))))
-    .catch(err => (dispatch(receiveErrors(err.response.data))))
-);
-
-export const editBusiness = businessData => dispatch => (
-    APIUtil.editBusiness(businessData)
-    .then(business => (dispatch(receiveOneBusiness(business))))
     .catch(err => (dispatch(receiveErrors(err.response.data))))
 );
