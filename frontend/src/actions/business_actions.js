f02391c6dc3c1efb02a836646503bc26a9b15b42
Josh Choi <joshuachoi0508@gmail.com>
2019-01-08 21:23:55 -0800

    Make changes to business actions

--- frontend/src/actions/business_actions.js
+++ frontend/src/actions/business_actions.js
@@ -2,6 +2,7 @@ import * as APIUtil from '../util/business_api_util';
 
 export const RECEIVE_ALL_BUSINESSES = "RECEIVE_ALL_BUSINESSES";
 export const RECEIVE_BUSINESS_ERRORS = "RECEIVE_BUSINESS_ERRORS";
+export const DELETE_ONE_BUSINESS = "DELETE_ONE_BUSINESS"
 
 
 export const receiveAllBusinesses = businesses => ({
@@ -13,6 +14,11 @@ export const receiveErrors = errors => ({
     type: RECEIVE_BUSINESS_ERRORS,
     errors
 });
+
+export const deleteOneBusiness = businessData => ({
+    type: DELETE_ONE_BUSINESS,
+    businessData
+})
 
 export const getBusinesses = () => dispatch => (
     APIUtil.getBusinesses().then(businesses => (
@@ -28,7 +34,6 @@ export const addBusiness = businessData => dispatch => (
 );
 
 export const removeBusiness = businessData => dispatch => {
-    return (APIUtil.removeBusiness(businessData)
-    .catch(err => (dispatch(receiveErrors(err.response.data))))
-    )
+    APIUtil.removeBusiness(businessData)
+    .then(dispatch(deleteOneBusiness(businessData)))
 };
--- frontend/src/reducers/business_reducer.js
+++ frontend/src/reducers/business_reducer.js
@@ -1,12 +1,16 @@
-import { RECEIVE_ALL_BUSINESSES } from '../actions/business_actions';
+import { RECEIVE_ALL_BUSINESSES, DELETE_ONE_BUSINESS } from '../actions/business_actions';
 import merge from 'lodash/merge';
 
 export default function (prevState = {}, action) {
     Object.freeze(prevState);
-    // let nextState = merge({}, prevState);
+    let nextState = merge({}, prevState);
     switch (action.type) {
         case RECEIVE_ALL_BUSINESSES:
             return merge({}, action.businesses.data);
+        case DELETE_ONE_BUSINESS:
+            let deleteKey = Number(Object.keys(nextState).filter(key => nextState[key]._id === action.businessData._id)[0])
+            delete nextState[deleteKey];
+            return merge({}, nextState);
         default:
             return prevState;
     }
