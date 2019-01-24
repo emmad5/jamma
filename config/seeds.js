f5797e49c59d6e148784ff5763d5fb12ba970f46
Emma Deas <emmadeas9@gmail.com>
2019-01-24 14:21:25 -0800

    seed

--- config/seeds.js
+++ config/seeds.js
@@ -211,7 +211,7 @@ const go = async () => {
         days: ["Sun", "Mon", "Tues", "Weds", "Thurs", "Fri", "Sat"],
         startTime: "13:00",
         endTime: "19:00",
-        imageUrl: "https://s3-media4.fl.yelpcdn.com/bphoto/z32SaxOMCYuOgnq9plzqDg/o.jpg"
+        imageUrl: "https://www.totalhappyhour.com/preview/500-450/content/files/mod.happyhour/72979621425d4068bdadc79c4a441f30.jpg"
     })
     const Belga = new Business({
         name: "Belga",
@@ -259,6 +259,21 @@ const go = async () => {
             ]
         },
         imageUrl: "https://cdn.vox-cdn.com/thumbor/27HMeujtUKjHyPX661NrX2vgsL4=/0x0:5472x3648/1200x0/filters:focal(0x0:5472x3648):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/10178401/Comstock_interior.jpg"
+    })
+    const Maybecks = new Business({
+        name: "Maybeck's",
+        address: "3213 Scott St San Francisco, CA 94123",
+        days: ["Tues", "Weds", "Thurs", "Fri", "Sat", "Sun"],
+        startTime: "16:30",
+        endTime: "18:00",
+        menu: {
+            "7": [
+                "Bar Bites",
+                "Well Drinks",
+                "Half Dozen Oysters"
+            ]
+        },
+        imageUrl: "https://cdn.vox-cdn.com/thumbor/0zJ09_dyzdi8-ZJHMiom84bbmRQ=/0x0:2000x1333/1870x1403/filters:focal(840x506:1160x826):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/62460434/SpaghettiBros_PChang-9274.0.0.0.0.0.jpg"
     })
     const FifthArrow = new Business({
         name: "Fifth Arrow",
@@ -354,6 +369,7 @@ const go = async () => {
     TheSaratoga.save().then(() => console.log("It worked!")).catch(() => console.log("It failed!"));
     ThePigAndWhistle.save().then(() => console.log("It worked!")).catch(() => console.log("It failed!"));
     ScopoDivino.save().then(() => console.log("It worked!")).catch(() => console.log("It failed!"));
+    Maybecks.save().then(() => console.log("It worked!")).catch(() => console.log("It failed!"));
 }
 
 go();
