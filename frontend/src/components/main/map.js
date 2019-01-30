38d7a91fc61ca36dd9348bcc2ac9d932650ee49e
Emma Deas <emmadeas9@gmail.com>
2019-01-30 14:15:57 -0800

    style hover name

--- frontend/src/components/main/map.js
+++ frontend/src/components/main/map.js
@@ -97,12 +97,12 @@ class Map extends React.Component {
                 marker.addListener('mouseover', function (event) {
                     let label = {
                         text: biz.name,
-                        color: 'black',
+                        color: '#8FB5B7',
                         labelClass: 'label',
-                        fontsize: 100,
-                        fontSize: "16px",
+                        fontSize: '20px',
+                        fontFamily: 'Symbol',
                         fontWeight: "bold",
-                        background: 'white',
+                     
                     }
                     this.setLabel(label);
                     // var icon = { 
